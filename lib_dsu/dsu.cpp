beeaa6c12c967d4bc9e4bccf068cd307986e12b3
Andrey-Baranov <dlyneero1@gmail.com>
2025-02-10 11:07:13 +0300

    Заполнен файл dsu.cpp

---
+++ lib_dsu/dsu.cpp
@@ -0,0 +1,35 @@
+#include "dsu.h"
+#include <stdexcept>
+
+DSU::DSU(int size) : _size(size) {
+    _parent = new int[size];
+    _rank = new int[size];
+    for (int i = 0; i < size; i++) {
+        make_set(i + 1);  // Индексация с 1
+    }
+}
+
+DSU::~DSU() {
+    delete[] _parent;
+    delete[] _rank;
+}
+
+void DSU::make_set(int elem) {
+    if (elem <= 0 || elem > _size) {
+        throw std::logic_error("Input Error: Element out of bounds.");
+    }
+    elem--;  // Приводим к нулевой индексации
+    _parent[elem] = elem;
+    _rank[elem] = 0;
+}
+
+int DSU::find(int elem) {
+    if (elem <= 0 || elem > _size) {
+        throw std::logic_error("Input Error: Element out of bounds.");
+    }
+    elem--;  // Приводим к нулевой индексации
+    if (_parent[elem] != elem) {
+        return find(_parent[elem]);  // Рекурсивный вызов
+    }
+    return elem;
+}
