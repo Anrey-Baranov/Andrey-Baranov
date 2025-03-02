9a95f031218175552428127137136996edce1407
Andrey-Baranov <dlyneero1@gmail.com>
2025-03-02 14:09:09 +0300

    Доисправлена ошибка в ITable

--- lib_itable/ITable.h
+++ lib_itable/ITable.h
@@ -1,8 +1,10 @@
 template<class TKey, class TVal>
 class ITable {
-    ITable(ITable const&);
-    ITable(TKey, TVal);
-    ITable();
+public:
+    ITable(ITable const&)= delete;
+    ITable(TKey, TVal) = delete;
+    ITable& operator=(ITable const&) = delete;
+    ITable() = default;
     virtual TKey insert(TVal value) = 0; 
     virtual void insert(TKey key, TVal value) = 0; 
     virtual void erase(TKey key) = 0; 
