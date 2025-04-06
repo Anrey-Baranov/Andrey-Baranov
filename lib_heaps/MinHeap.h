2416a9646b69763183f7f41f3c141ff88ec187e7
Anrey-Baranov <dlyneero1@gmail.com>
2025-04-06 13:08:38 +0300

    Создана библиотека Хеш-тиблицы

--- lib_heaps/MinHeap.h
+++ lib_heaps/MinHeap.h
@@ -10,6 +10,25 @@ protected:
         return a < b;
     }
 
+    void heapify(size_t i) override {
+        size_t size = this->data.size();
+        if (i >= size) return;
+
+        while (true) {
+            size_t smallest = i;
+            size_t l = this->left(i);
+            size_t r = this->right(i);
+
+            if (l < size && this->data[l] < this->data[smallest]) smallest = l;
+            if (r < size && this->data[r] < this->data[smallest]) smallest = r;
+
+            if (smallest == i) break;
+
+            std::swap(this->data[i], this->data[smallest]);
+            i = smallest;
+        }
+    }
+
 public:
     MinHeap() = default;
     explicit MinHeap(const std::vector<T>& array) : Heap<T>(array) {
